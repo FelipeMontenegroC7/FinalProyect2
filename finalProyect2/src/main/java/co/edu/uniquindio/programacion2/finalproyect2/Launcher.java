59fba5e50f54b7bb32d651921c445b207fe248e6
Angelinho <angeld.diazp@uqvirtual.edu.co>
2025-10-30 18:44:41 -0500

    Delete finalProyect2 directory

--- finalProyect2/src/main/java/co/edu/uniquindio/programacion2/finalproyect2/Launcher.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.programacion2.finalproyect2;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
